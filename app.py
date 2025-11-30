0f752eebd639bab9a0be7f1fe33221a061ad6412
Neikov Daniil <nejkovdaniil@gmail.com>
2025-11-30 14:25:24 +0000

    fix: api handler

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/api')
+def api():
+    return jsonify({"message": "Hello from backend"}), 200
 
 @app.route("/health")
 def health():
