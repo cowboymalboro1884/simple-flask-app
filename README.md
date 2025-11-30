6884520e5eed1d0628730ad679044b12169db679
Neikov Daniil <nejkovdaniil@gmail.com>
2025-11-30 13:15:15 +0000

    feat flask-app: add docker image

---
+++ README.md
@@ -0,0 +1,5 @@
+Simple flask app with /health and /ready handlers
+
+Used for k8s + containers learning
+
+See https://hub.docker.com/r/cowboymarlboro1884/simple-flask-app 
---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+@app.route("/ready")
+def ready():
+    return "READY", 200
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
